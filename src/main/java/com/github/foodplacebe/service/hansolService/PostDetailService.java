3242f9147b4deb8e85bb5914a32665b98db18bde
HanSol <wkdqhrh010@naver.com>
2024-01-22 10:32:23 +0900

    feat: post-favorite-status(ì¡좋아요 등록 여부 ì확인 로직 추가)

--- src/main/java/com/github/foodplacebe/service/hansolService/PostDetailService.java
+++ src/main/java/com/github/foodplacebe/service/hansolService/PostDetailService.java
@@ -31,8 +31,10 @@ public class PostDetailService {
     public ResponseDto getPostDetails(Integer postId) {
         Posts post = postsJpa.findById(postId)
                 .orElseThrow(()-> new NotFoundException("게시물을 찾을 수 없습니다.", postId));
-        post.setViewCount(post.getViewCount() + 1);
+
+        post.setViewCount(post.getViewCount() + 1); // 조회수 +1
         postsJpa.save(post);
+
         List<PostPhotos> postPhotos = post.getPostPhotos();
         Integer favoriteCount = (int) post.getPostFavorites().stream().count();
 
@@ -63,7 +65,16 @@ public class PostDetailService {
             postFavoriteJpa.deleteByUserEntityAndPosts(userEntity, posts);
             return new ResponseDto(200, "좋아요 해제", "postId : " + postId + ", userId : " + userId);
         }
+    }
 
+    public ResponseDto getFavoriteStatus (CustomUserDetails customUserDetails, Integer postId) {
+        Posts posts = postsJpa.findById(postId)
+                .orElseThrow(() -> new NotFoundException("게시물을 찾을 수 없습니다.", postId));
+        UserEntity userEntity = userJpa.findById(customUserDetails.getUserId())
+                .orElseThrow(()-> new NotFoundException("유저정보를 찾을 수 없습니다.", customUserDetails.getUserId()));
 
+        Boolean favoriteStatus = postFavoriteJpa.findByUserEntityAndPosts(userEntity, posts) != null;
+
+        return new ResponseDto(200, "좋아요 등록 여부 확인", favoriteStatus);
     }
 }
