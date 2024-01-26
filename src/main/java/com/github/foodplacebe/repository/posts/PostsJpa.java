f0890dba09202912d28862319c10085054138acd
gunbamm <56660732+gunbamm@users.noreply.github.com>
2024-01-26 20:00:56 +0900

    [feature-jiyun]
    [추가] 맛집 상세페이지 기능 구현

--- src/main/java/com/github/foodplacebe/repository/posts/PostsJpa.java
+++ src/main/java/com/github/foodplacebe/repository/posts/PostsJpa.java
@@ -1,8 +1,11 @@
 package com.github.foodplacebe.repository.posts;
 
+import com.github.foodplacebe.repository.users.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostsJpa extends JpaRepository<Posts, Integer> {
+
+    Posts findByUserEntityAndPostId(UserEntity userEntity,Integer postId);
 }
