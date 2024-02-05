9586331bcfd53277f0f0e59c29c2265590ec8009
신한솔 <shinhansol@OneSol-MacBook.local>
2024-02-05 15:15:39 +0900

    feat: 회원정보 로직 구현

---
+++ src/main/java/com/github/foodplacebe/web/dto/account/UpdateMyInfoRequest.java
@@ -0,0 +1,19 @@
+package com.github.foodplacebe.web.dto.account;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateMyInfoRequest {
+    private String nickName;
+    private String phoneNum;
+    private String neighborhood;
+    private String gender;
+    private String dateOfBirth;
+}
