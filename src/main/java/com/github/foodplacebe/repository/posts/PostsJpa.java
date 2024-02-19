5ae634ab2e4a00d819d305fbf2c26145884549af
신한솔 <shinhansol@OneSol-MacBook.local>
2024-02-19 17:26:21 +0900

    fix: 필요없는 파일 제거

--- src/main/java/com/github/foodplacebe/repository/posts/PostsJpa.java
+++ src/main/java/com/github/foodplacebe/repository/posts/PostsJpa.java
@@ -16,18 +16,6 @@ import java.util.List;
 public interface PostsJpa extends JpaRepository<Posts, Integer> {
 
     Posts findByUserEntityAndPostId(UserEntity userEntity,Integer postId);
-    @Query(
-            "SELECT new com.github.foodplacebe.web.dto.hansolDto.FindPostsResponse(" +
-                    "p.postId, p.name, p.neighborhood, p.category, p.menu, p.viewCount, p.mainPhoto, p.createAt, SIZE(p.postFavorites)) " +
-                    "FROM Posts p " +
-                    "WHERE p.neighborhood IN ?1 AND p.category IN ?2 " +
-                    "GROUP BY p.postId " +
-                    "ORDER BY p.createAt DESC"
-    )
-    Page<FindPostsResponse> findPostsByAreaAndCategoryAndOrderOrderByCreateAt(List<String> areaList, List<String> categoryList, Pageable pageable);
-
-
-    Page<FindPostsResponse> findTopByOrderByViewCountDesc(int count, Pageable pageable);
 
     @Query("SELECT p FROM Posts p WHERE p.address LIKE CONCAT('%', :city, '%')")
     Page<Posts> findRestaurantsByCity(@Param("city") String city, Pageable pageable);
